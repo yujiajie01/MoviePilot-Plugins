3efc1cff0a6cb3f7021f3c3e41509fdabe8f830b
xijin285 <162889763+xijin285@users.noreply.github.com>
2025-07-03 01:13:44 +0800

    添加随机图库API插件依赖

--- plugins.v2/randompic/__init__.py
+++ plugins.v2/randompic/__init__.py
@@ -118,7 +118,7 @@ class RandomPic(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/xijin285/MoviePilot-Plugins/refs/heads/main/icons/randompic.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.0.0"
     # 插件作者
     plugin_author = "M.Jinxi"
     # 作者主页
